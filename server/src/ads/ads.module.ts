4a2e85448019b012d9678c070cbe6d2433d99cef
Giuseppe Dias <giuseppe.dias17@gmail.com>
2023-02-18 14:47:26 -0300

    feat: add entities in respective modules

--- server/src/ads/ads.module.ts
+++ server/src/ads/ads.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AdsService } from './ads.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AdsController } from './ads.controller';
+import { AdsService } from './ads.service';
+import { Ad } from './entities/ad.entity';
+import { Image } from './entities/image.entity';
+import { Vehicle } from './entities/vehicle.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Ad, Image, Vehicle])],
   controllers: [AdsController],
   providers: [AdsService],
 })
