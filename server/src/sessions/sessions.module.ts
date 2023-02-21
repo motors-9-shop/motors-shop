5d661e6b221e63ffc3b8a2aeaac2dfd64e10ebb1
Giuseppe Dias <giuseppe.dias17@gmail.com>
2023-02-21 09:35:26 -0300

    feat: add login route

--- server/src/sessions/sessions.module.ts
+++ server/src/sessions/sessions.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
-import { SessionsService } from './sessions.service';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from '../users/users.module';
 import { SessionsController } from './sessions.controller';
+import { SessionsService } from './sessions.service';
+import { JwtStategy } from './strategies';
 
 @Module({
+  imports: [
+    UsersModule,
+    PassportModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    JwtModule.register({
+      secret: process.env.SECRET_KEY,
+    }),
+  ],
   controllers: [SessionsController],
-  providers: [SessionsService],
+  providers: [SessionsService, JwtStategy],
 })
 export class SessionsModule {}
