5e5a1d6a49e1d06890be676bfbebf6a79b6dfb82
Wilson Mesquita <wilsonbrasilplauforever@gmail.com>
2023-02-24 16:25:47 -0300

    feat: header responsive with logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
+import Header from "./components/Header";
+
 const App = () => {
-  return <div className="App"></div>;
+  return <div className="App">
+    <Header />
+  </div>;
 };
 
 export default App;
