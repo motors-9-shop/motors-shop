fd853c32066fd11324627f1d49c380ad80a8c6cd
Giuseppe Dias <giuseppe.dias17@gmail.com>
2023-02-26 16:51:26 -0300

    fix: change transformAdList to list correctly

--- server/src/utils/tranformAdList.util.ts
+++ server/src/utils/tranformAdList.util.ts
@@ -2,18 +2,24 @@ import { Ad } from '../ads/entities/ad.entity';
 
 export const tranformAdList = (value: Ad[]) => {
   return value.reduce((adList, ad) => {
+    let list;
+
     if (!(ad.type in adList)) {
-      adList[ad.type] =
-        ad.type === 'auction'
-          ? []
-          : {
-              [ad.vehicle.type]: [],
-            };
+      adList[ad.type] = ad.type === 'auction' ? [] : {};
     }
 
-    const objectAlias = adList[ad.type];
-    const list =
-      ad.type === 'auction' ? objectAlias : objectAlias[ad.vehicle.type];
+    list = adList[ad.type];
+
+    if (ad.type === 'auction') {
+      list.push(ad);
+      return adList;
+    }
+
+    if (!(ad.vehicle.type in list)) {
+      list[ad.vehicle.type] = [];
+    }
+
+    list = list[ad.vehicle.type];
 
     list.push(ad);
 
