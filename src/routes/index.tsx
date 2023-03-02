2b0299c9faf51e6ba8f5e4f014b4c2cd1529a9ac
Ygor José <98772319+ygorjg@users.noreply.github.com>
2023-03-02 18:36:55 -0300

    feat: created login layout

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,14 @@
-import { Routes, Route, Navigate } from "react-router-dom"
-import Home from "../pages/Home"
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
 
 const RoutesMain = () => {
   return (
-      <Routes>
-          <Route path="/" element={<Home />}/>
-      </Routes>
-  )
-}
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
+};
 
-export default RoutesMain
+export default RoutesMain;
