cbfb312a5167178d37c5edb09ad022ebfc93689d
Giuseppe Dias <giuseppe.dias17@gmail.com>
2023-03-04 11:04:29 -0300

    feat: create mailer global module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AdsModule } from './ads/ads.module';
 import { CommentsModule } from './comments/comments.module';
+import { MailerModule } from './mailer/mailer.module';
 import { SessionsModule } from './sessions/sessions.module';
 import { UsersModule } from './users/users.module';
 
@@ -16,10 +18,12 @@ import { UsersModule } from './users/users.module';
       url: process.env.DATABASE_URL,
       autoLoadEntities: true,
     }),
+    ScheduleModule.forRoot(),
     UsersModule,
     SessionsModule,
     AdsModule,
     CommentsModule,
+    MailerModule,
   ],
 })
 export class AppModule {}
