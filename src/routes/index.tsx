4f4fb6e44ce259ec9e57ed9ff58e9f34a67602e8
Wilson Mesquita <wilsonbrasilplauforever@gmail.com>
2023-03-05 18:25:31 -0300

    feat: redirect to home when not passing id in profile

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,15 @@
 import { Routes, Route, Navigate } from "react-router-dom"
 import Home from "../pages/Home"
+import Profile from "../pages/Profile"
 
 const RoutesMain = () => {
   return (
       <Routes>
           <Route path="/" element={<Home />}/>
+          <Route path="profile">
+            <Route path=":userId" element={<Profile />} />
+            <Route index element={<Navigate to="/" />} />
+          </Route>
       </Routes>
   )
 }
