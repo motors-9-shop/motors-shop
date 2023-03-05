be48883f3904b90ee03d3cca8aee666e001cc34c
Wilson Mesquita <wilsonbrasilplauforever@gmail.com>
2023-03-04 23:57:11 -0300

    feat: add header username by user authenticated

--- src/components/UserCard/index.tsx
+++ src/components/UserCard/index.tsx
@@ -1,18 +1,27 @@
 import { Box, BoxProps, Text } from "@chakra-ui/layout"
 import { Avatar, forwardRef } from "@chakra-ui/react"
 
-const UserCard = forwardRef<BoxProps, 'div'>((props, ref) => (
-    <Box
-        display="flex"
-        alignItems="center"
-        gap="16px"
-        ref={ref}
-        {...props}
-        cursor="pointer"
-    >
-        <Avatar name="Wilson Mesquita" size="sm"/>
-        <Text textStyle="body-1-400" color="grey.2">Wilson Mesquita</Text>
-    </Box>
-))
+interface IUserCardProps extends BoxProps {
+    ref?: React.Ref<HTMLDivElement>
+    username: string
+}
+
+const UserCard = forwardRef<IUserCardProps, 'div'>((props, ref) => {
+    const { username, ...rest } = props
+
+    return (
+        <Box
+            display="flex"
+            alignItems="center"
+            gap="16px"
+            ref={ref}
+            {...rest}
+            cursor="pointer"
+        >
+            <Avatar name={username} size="sm"/>
+            <Text textStyle="body-1-400" color="grey.2">{username}</Text>
+        </Box>
+    )
+})
 
 export default UserCard
