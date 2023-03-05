a4f79c67492e0e2291b6497b98a834509edb7f9f
Giuseppe Dias <giuseppe.dias17@gmail.com>
2023-03-05 17:56:01 -0300

    feat: use classSerializer as global interceptor

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,6 +1,5 @@
 import {
   Body,
-  ClassSerializerInterceptor,
   Controller,
   Delete,
   Get,
@@ -9,7 +8,6 @@ import {
   Patch,
   Post,
   SerializeOptions,
-  UseInterceptors,
 } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -17,7 +15,6 @@ import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 
 @Controller('users')
-@UseInterceptors(ClassSerializerInterceptor)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
