72f9995074fcca05f5ac04dae5477adb3675f6cb
Wilson Mesquita <wilsonbrasilplauforever@gmail.com>
2023-03-05 18:17:46 -0300

    style: add scroll on resolutions > 720px

--- src/components/AdList/index.tsx
+++ src/components/AdList/index.tsx
@@ -1,26 +1,38 @@
-import { HStack, SimpleGrid, Stack } from "@chakra-ui/react"
+import { Box, Flex, HStack, SimpleGrid, Stack, Text, useMediaQuery } from "@chakra-ui/react"
 import { IAd } from "../../interfaces"
 import AdCard from "../AdCard"
 
 interface IAdListProps {
     adList: IAd[]
+    title: string
+    badge?: boolean
 }
 
-const AdList = ({adList}: IAdListProps) => {
+const AdList = ({adList, title, badge}: IAdListProps) => {
+    const [isSmallerThan720] = useMediaQuery('(max-width: 720px)')
+
     return (
-        <HStack 
-            spacing={8} 
-            overflowX="scroll"
-            css={{
-                '&::-webkit-scrollbar': {
-                  display: 'none',
-                }
-              }}
+        <Flex
+            flexDir="column"
+            padding={isSmallerThan720 ? "2rem 0" : "80px 0"}
+            gap="2rem"
         >
-            {
-                adList?.map(ad => <AdCard ad={ad} key={ad.id}/>) 
-            }
-        </HStack>
+            <Text textStyle="heading-5-600">{title}</Text>
+            <HStack 
+                spacing={8} 
+                overflowX="scroll"
+                css={ isSmallerThan720 && {
+                    '&::-webkit-scrollbar': {
+                    display: 'none',
+                    }
+                }}
+            >
+                {
+                    adList?.map(ad => <AdCard ad={ad} key={ad.id} badge={badge} />) 
+                }
+            </HStack>
+        </Flex>
+
     )
 }
 
