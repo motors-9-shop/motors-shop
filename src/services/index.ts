7b87e32f67f9332b32d5654160892db96ff366b0
Wilson Mesquita <wilsonbrasilplauforever@gmail.com>
2023-03-05 18:20:13 -0300

    feat: link create ad route

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 
+export * from "./createAd"
 export * from "./getAllAd"
+export * from "./getProfile"
+export * from "./getProfileByToken"
 
 export const api = axios.create({
     baseURL: "/api",
