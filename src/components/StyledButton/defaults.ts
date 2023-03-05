1826ef14712afd0f41912147638b43a46a2bb673
Wilson Mesquita <wilsonbrasilplauforever@gmail.com>
2023-03-04 23:55:54 -0300

    style: add button variant styles from figma

---
+++ src/components/StyledButton/defaults.ts
@@ -0,0 +1,91 @@
+export const defaults = {
+    grey: {
+      colorScheme: "grey",
+      bg: "grey.0",
+      _hover: {
+        bg: "grey.1"
+      }
+    },
+    negative: {
+      colorScheme: "grey",
+      bg: "grey.6",
+      color: "grey.2",
+      _hover: {
+        bg: "grey.5"
+      }
+    },
+    brand: {
+      colorScheme: "brand",
+      bg: "brand.1",
+      color: "whiteFixed",
+      _hover: {
+        bg: "brand.2"
+      },
+      _disable: {
+        bg: "brand.3",
+        color: "brand.4"
+      }
+    },
+    brandOpacity: {
+      colorSchema: "brand",
+      bg: "brand.4",
+      color: "brand.1",
+    },
+    light: {
+      colorSchema: "grey",
+      bg: "grey.10",
+      color: "grey.1",
+    },
+    medium44: {
+      colorSchema: "grey",
+      bg: "grey.1",
+      color: "grey.10",
+      borderColor: "grey.0",
+      _hover: {
+        bg: "grey.9",
+      },
+    },
+    outline: {
+        colorSchema: "grey",
+        bg: "transparent",
+        color: "grey.0",
+        borderColor: "grey.0",
+        _hover: {
+            color: "grey.10",
+            bg: "grey.0"
+        }
+    },
+    outlineBrand1: {
+      colorSchema: "brand",
+      bg: "transparent",
+      color: "brand.1",
+      borderColor: "brand.1",
+      _hover: {
+        bg: "brand.4",
+      }
+    },
+    link: {
+      colorSchema: "grey",
+      bg: "transparent",
+      color: "grey.0",
+      _hover: {
+        bg: "grey.8",
+      },
+    },
+    alert: {
+      colorScheme: "feedback.alert",
+      bg: "feedback.alert.3",
+      color: "feedback.alert.1",
+      _hover: {
+        bg: "feedback.alert.2"
+      }
+    },
+    sucess: {
+      colorScheme: "feedback.sucess",
+      bg: "feedback.sucess.3",
+      color: "feedback.sucess.1",
+      _hover: {
+        bg: "feedback.sucess.2"
+      }
+    } 
+  }
---
+++ src/components/StyledButton/index.tsx
@@ -0,0 +1,27 @@
+import { Button, ButtonProps } from "@chakra-ui/react";
+import { defaults } from "./defaults";
+
+interface StyledButtonProps extends ButtonProps {
+  variant: "grey" | "negative" | "brand" | "brandOpacity" | "light" | "medium44" | "outlineBrand1" | "link" | "alert" | "sucess" | "outline";
+}
+
+const StyledButton = ({ variant, children, ...rest }: StyledButtonProps) => {
+  return (
+    <Button 
+      textStyle="button-big-text"
+      borderRadius={"4px"}
+      _disabled={{
+        colorScheme: "grey",
+        bg: "grey.5",
+        color: "whiteFixed",
+      }}
+      border={"1.5px solid transparent"} 
+      {...defaults[variant]} 
+      {...rest} 
+    >
+      {children}
+    </Button>
+  );
+};
+
+export default StyledButton;
