997cd30faf2db401595e19df1b80c3d1674d72b6
Ygor José <98772319+ygorjg@users.noreply.github.com>
2023-03-09 10:51:34 -0300

    feat: created signup page layout

--- src/components/FormLogin/index.tsx
+++ src/components/FormLogin/index.tsx
@@ -9,79 +9,95 @@ import {
   useMediaQuery,
 } from "@chakra-ui/react";
 
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
+interface IData {
+  name: string;
+  password: string;
+}
+
 const FormLogin = () => {
   const [isSmallerThan720] = useMediaQuery("(max-width: 720px)");
 
   return (
-    <FormControl
-      backgroundColor="#FDFDFD"
-      borderRadius="4px"
-      p={isSmallerThan720 ? "2px 30px" : "60px 60px"}
-    >
-      <InputGroup flexDirection="column" gap="15px">
-        <Box>
-          <Text as="h2" textStyle="heading-5-600">
-            Login
-          </Text>
-        </Box>
-        <Box>
-          <FormLabel htmlFor="user" textStyle="input-label">
-            Usuário
-          </FormLabel>
-          <Input
-            placeholder="Digitar usuário"
-            name="user"
-            id="user"
-            size="md"
-            borderRadius="4px"
-            width="100%"
-          />
-        </Box>
-        <Box>
-          <FormLabel htmlFor="password" textStyle="input-label">
-            Senha
-          </FormLabel>
-          <Input
-            placeholder="Digitar senha"
-            name="password"
-            id="password"
-            size="md"
-            borderRadius="4px"
-            width="100%"
-          />
-        </Box>
-        <Box display="flex" justifyContent="flex-end">
-          <Text textStyle="body-2-500" cursor="pointer">
-            Esqueci minha senha
-          </Text>
-        </Box>
-        <Box
-          display="flex"
-          flexDir="column"
-          justifyContent="center"
-          alignItems="center"
-          gap="1.5625rem"
-        >
+    <form>
+      <FormControl
+        backgroundColor="#FDFDFD"
+        borderRadius="4px"
+        p={isSmallerThan720 ? "2px 30px" : "60px 60px"}
+      >
+        <InputGroup flexDirection="column" gap="15px">
           <Box>
-            <Button
-              textStyle="button-big-text"
-              color="#FFF"
-              backgroundColor="#4529E6"
-              p={isSmallerThan720 ? "20px 100px" : "20px 130px"}
-            >
-              Entrar
-            </Button>
+            <Text as="h2" textStyle="heading-5-600">
+              Login
+            </Text>
           </Box>
-          <Text textStyle="body-2-400">Ainda não possui uma conta?</Text>
-          <Button
-            p={isSmallerThan720 ? "20px 90px" : "20px 120px"}
-            marginBottom="5px"
+          <Box>
+            <FormLabel htmlFor="name" textStyle="input-label">
+              Usuário
+            </FormLabel>
+            <Input
+              placeholder="Digitar usuário"
+              id="name"
+              size="md"
+              borderRadius="4px"
+              width="100%"
+            />
+          </Box>
+          <Box>
+            <FormLabel htmlFor="password" textStyle="input-label">
+              Senha
+            </FormLabel>
+            <Input
+              placeholder="Digitar senha"
+              id="password"
+              size="md"
+              borderRadius="4px"
+              width="100%"
+            />
+          </Box>
+          <Box display="flex" justifyContent="flex-end">
+            <Text textStyle="body-2-500" cursor="pointer">
+              Esqueci minha senha
+            </Text>
+          </Box>
+          <Box
+            display="flex"
+            flexDir="column"
+            justifyContent="center"
+            alignItems="center"
+            gap="1.5625rem"
           >
-            Cadastrar
-          </Button>
-        </Box>
-      </InputGroup>
-    </FormControl>
+            <Box>
+              <Button
+                textStyle="button-big-text"
+                color="#FFF"
+                backgroundColor="#4529E6"
+                p={isSmallerThan720 ? "20px 100px" : "20px 130px"}
+                type="submit"
+              >
+                Entrar
+              </Button>
+            </Box>
+            <Text textStyle="body-2-400">Ainda não possui uma conta?</Text>
+            <Link to="/signup">
+              <Button
+                p={isSmallerThan720 ? "20px 90px" : "20px 120px"}
+                marginBottom="5px"
+                type="button"
+              >
+                Cadastrar
+              </Button>
+            </Link>
+          </Box>
+        </InputGroup>
+      </FormControl>
+    </form>
   );
 };
 
